16b022e072693b6dd5edae472ebd3f8f49ff01dc
Gustavo Diaz <diazchg@gmail.com>
2020-07-07 16:04:39 -0300

    terminada verificacion de adn

--- api/controllers/mutant_controller.py
+++ api/controllers/mutant_controller.py
@@ -1,11 +1,98 @@
 class MutantController:
+    MUTANT_SEQUENCE_SIZE = 4
 
     def __init__(self, dna):
         self._dna = dna
 
     def is_mutant(self):
-        print(self._dna)
-        dna_list = self._dna.get("dna")
-        print("".join(dna_list))
+        is_mutant = False
+        sequences = self._decompose_dna()
+        count = self._count_mutant_sequences(sequences)
+        print("cantidad de mutantes: ", count)
+        if count > 1:
+            is_mutant = True
+        return is_mutant
 
+    def _decompose_dna(self):
+        sequences = []
+        sequences.extend(self._dna)
+        sequences.extend(self._decompose_vertically(self._dna))
+        sequences.extend(self._decompose_diagonally(self._dna))
+        sequences.extend(self._decompose_diagonally_but_backwards(self._dna))
+        return sequences
+
+    def _decompose_vertically(self, dna):
+        dna_vertors_len = len(dna)
+        vertical_sequences = []
+        for i in range(dna_vertors_len):
+            temporal_vertical_seq = ""
+            for sequences in dna:
+                temporal_vertical_seq += sequences[i]
+
+            vertical_sequences.append(temporal_vertical_seq)
+        return vertical_sequences
+
+    def _decompose_diagonally(self, dna):
+        sequences = []
+        middle_diagonal = self._decompose_middle_diagonal(dna)
+        down_diagonals = self._decompose_lower_diagonals(dna[1:])
+        upper_diagonals = self._decompose_upper_diagonals([x[1:] for x in dna[:-1]])
+        sequences.append(middle_diagonal)
+        sequences.extend(down_diagonals)
+        sequences.extend(upper_diagonals)
+        return sequences
+
+    def _decompose_middle_diagonal(self, dna):
+        temp_sequence = ""
+        for i, seq in enumerate(dna):
+            temp_sequence += seq[i]
+        return temp_sequence
+
+    def _decompose_upper_diagonals(self, dna):
+        diagonal_list = [self._decompose_middle_diagonal(dna)]
+        if len(dna) > 1:
+            diagonal_list.extend(self._decompose_upper_diagonals([x[1:] for x in dna[:-1]]))
+        return diagonal_list
+
+    def _decompose_lower_diagonals(self, dna):
+        diagonal_list = [self._decompose_middle_diagonal(dna)]
+        if len(dna) > 1:
+            diagonal_list.extend(self._decompose_lower_diagonals(dna[1:]))
+        return diagonal_list
+
+    def _decompose_diagonally_but_backwards(self, dna):
+        reversed_dna = self._reverse_sequences(dna)
+        return self._decompose_diagonally(reversed_dna)
+
+    def _reverse_sequences(self, sequences):
+        reversed_sequences = []
+        for sequence in sequences:
+            reversed_sequences.append("".join(reversed(sequence)))
+        return reversed_sequences
+
+    def _count_mutant_sequences(self, sequences):
+        verified_mutant_sequences_count = 0
+        for sequence in sequences:
+            verified_mutant_sequences_count += self._verify_mutant_sequence(sequence)
+        return verified_mutant_sequences_count
+
+    def _verify_mutant_sequence(self, sequence):
+        found_base_count = 0
+        sequences_found = 0
+        last_base = None
+        sequence_len = len(sequence)
+        if sequence_len >= self.MUTANT_SEQUENCE_SIZE:
+            for i, base in enumerate(sequence):
+                if i == 0:
+                    last_base = base
+                    continue
+                if last_base == base:
+                    found_base_count += 1
+                else:
+                    found_base_count = 0
+                if found_base_count == self.MUTANT_SEQUENCE_SIZE - 1:
+                    sequences_found += 1
+                    found_base_count = 0
+                last_base = base
+        return sequences_found
 
---
+++ api/controllers/tests/test_mutant_controller.py
@@ -0,0 +1,141 @@
+from unittest import TestCase
+from api.controllers.mutant_controller import MutantController
+
+
+class TestMutantController(TestCase):
+
+    def setUp(self):
+        self.dna = ['ATGCGA',
+                    'CAGTGC',
+                    'TTATGT',
+                    'AGAAGG',
+                    'CCCCTA',
+                    'TCACTG']
+
+    def test_is_mutant(self):
+        dna = ['TCACTG',
+                'TCACGG',
+                'TCAGTG',
+                'TCGCTG',
+                'TCACTG',
+                'TCACTG']
+
+        mc = MutantController(dna)
+        self.assertTrue(mc.is_mutant())
+
+    def test__decompose_dna(self):
+        mc = MutantController(self.dna)
+        mc._decompose_dna()
+
+    def test__decompose_vertically(self):
+        mc = MutantController(self.dna)
+        vertical_sequences = mc._decompose_vertically(self.dna)
+        self.assertEqual(6, len(vertical_sequences))
+        self.assertEqual('ACTACT', vertical_sequences[0])
+        self.assertEqual('TATGCC', vertical_sequences[1])
+        self.assertEqual('GGAACA', vertical_sequences[2])
+        self.assertEqual('CTTACC', vertical_sequences[3])
+        self.assertEqual('GGGGTT', vertical_sequences[4])
+        self.assertEqual('ACTGAG', vertical_sequences[5])
+
+    def test__decompose_lower_diagonals(self):
+        mc = MutantController(self.dna)
+        diagonal_sequences = mc._decompose_lower_diagonals(self.dna[1:])
+        self.assertEqual(5, len(diagonal_sequences))
+        self.assertEqual('CTACT', diagonal_sequences[0])
+        self.assertEqual('TGCC', diagonal_sequences[1])
+        self.assertEqual('ACA', diagonal_sequences[2])
+        self.assertEqual('CC', diagonal_sequences[3])
+        self.assertEqual('T', diagonal_sequences[4])
+
+    def test__decompose_upper_diagonals(self):
+        dna = ['ATGCGA',
+               'CAGTGC',
+               'TTATGT',
+               'AGAAGG',
+               'CCCCTA',
+               'TCACTG']
+
+        mc = MutantController(self.dna)
+        diagonal_sequences = mc._decompose_upper_diagonals([x[1:] for x in dna[:-1]])
+        self.assertEqual(5, len(diagonal_sequences))
+        self.assertEqual('TGTGA', diagonal_sequences[0])
+        self.assertEqual('GTGG', diagonal_sequences[1])
+        self.assertEqual('CGT', diagonal_sequences[2])
+        self.assertEqual('GC', diagonal_sequences[3])
+        self.assertEqual('A', diagonal_sequences[4])
+
+    def test__decompose_middle_diagonals(self):
+        mc = MutantController(self.dna)
+        vertical_sequences = mc._decompose_middle_diagonal(self.dna)
+        self.assertEqual(6, len(vertical_sequences))
+        self.assertEqual('AAAATG', vertical_sequences)
+
+    def test__reverse_sequences(self):
+        mc = MutantController(self.dna)
+        reversed_aequences = mc._reverse_sequences(self.dna)
+        self.assertEqual(6, len(reversed_aequences))
+        self.assertEqual('AGCGTA', reversed_aequences[0])
+        self.assertEqual('CGTGAC', reversed_aequences[1])
+        self.assertEqual('TGTATT', reversed_aequences[2])
+        self.assertEqual('GGAAGA', reversed_aequences[3])
+        self.assertEqual('ATCCCC', reversed_aequences[4])
+        self.assertEqual('GTCACT', reversed_aequences[5])
+
+    def test__decompose_diagonally(self):
+        mc = MutantController(self.dna)
+        diagonal_sequences = mc._decompose_diagonally(self.dna)
+        self.assertEqual(11, len(diagonal_sequences))
+        self.assertEqual('AAAATG', diagonal_sequences[0])
+        self.assertEqual('CTACT', diagonal_sequences[1])
+        self.assertEqual('TGCC', diagonal_sequences[2])
+        self.assertEqual('ACA', diagonal_sequences[3])
+        self.assertEqual('CC', diagonal_sequences[4])
+        self.assertEqual('T', diagonal_sequences[5])
+        self.assertEqual('TGTGA', diagonal_sequences[6])
+        self.assertEqual('GTGG', diagonal_sequences[7])
+        self.assertEqual('CGT', diagonal_sequences[8])
+        self.assertEqual('GC', diagonal_sequences[9])
+        self.assertEqual('A', diagonal_sequences[10])
+
+    def test__decompose_diagonally_but_backwards(self):
+        mc = MutantController(self.dna)
+        diagonal_sequences = mc._decompose_diagonally_but_backwards(self.dna)
+        self.assertEqual(11, len(diagonal_sequences))
+        self.assertEqual('AGTACT', diagonal_sequences[0])
+        self.assertEqual('CGACC', diagonal_sequences[1])
+        self.assertEqual('TGCA', diagonal_sequences[2])
+        self.assertEqual('GTC', diagonal_sequences[3])
+        self.assertEqual('AT', diagonal_sequences[4])
+        self.assertEqual('G', diagonal_sequences[5])
+        self.assertEqual('GTAGC', diagonal_sequences[6])
+        self.assertEqual('CGTA', diagonal_sequences[7])
+        self.assertEqual('GAT', diagonal_sequences[8])
+        self.assertEqual('TC', diagonal_sequences[9])
+        self.assertEqual('A', diagonal_sequences[10])
+
+    def test_verify_one_found_mutant_sequence(self):
+        mc = MutantController(self.dna)
+        self.assertEqual(1, mc._verify_mutant_sequence('CCCCTA'))
+
+    def test_verify_two_found_mutant_sequence(self):
+        mc = MutantController(self.dna)
+        self.assertEqual(2, mc._verify_mutant_sequence('CCCCTTTTA'))
+
+    def test_verify_zero_found_mutant_sequence(self):
+        mc = MutantController(self.dna)
+        self.assertEqual(0, mc._verify_mutant_sequence('CCACCTTATT'))
+
+    def test_verify_many_found_mutant_sequence(self):
+        mc = MutantController(self.dna)
+        self.assertEqual(0, mc._verify_mutant_sequence('CCACCTTATT'))
+        self.assertEqual(1, mc._verify_mutant_sequence('CCCCTA'))
+        self.assertEqual(2, mc._verify_mutant_sequence('CCCCTTTTA'))
+        self.assertEqual(4, mc._verify_mutant_sequence('CCCCTACCCCTACCCCTTTTA'))
+
+    def test_verify_zero_found_mutant_short_sequence(self):
+        mc = MutantController(self.dna)
+        self.assertEqual(0, mc._verify_mutant_sequence('CCA'))
+        self.assertEqual(0, mc._verify_mutant_sequence('CCAA'))
+        self.assertEqual(0, mc._verify_mutant_sequence('C'))
+        self.assertEqual(0, mc._verify_mutant_sequence(''))
